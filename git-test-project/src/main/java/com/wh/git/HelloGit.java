41ba1965259403ccd520e73a5839de58fa7cc721
wadamancy <wadamancy1111@163.com>
2020-03-03 10:32:06 +0800

    本地工程添加到git

---
+++ git-test-project/src/main/java/com/wh/git/HelloGit.java
@@ -0,0 +1,10 @@
+package com.wh.git;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
