f3f2aaa162f0424b610b5b5ca88fbe56fb9c2e14
wadamancy <wadamancy1111@163.com>
2020-03-03 10:43:20 +0800

    修改内容

--- git-test-project/src/main/java/com/wh/git/HelloGit.java
+++ git-test-project/src/main/java/com/wh/git/HelloGit.java
@@ -7,6 +7,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("hello git");
 
-        System.out.println("111111111111111");
+        System.out.println("2222222222");
     }
 }
